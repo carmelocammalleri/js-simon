5e905221ab97915d95bc7fceaf75d89e3be7464f
Melozzo <carmecamma1995@gmail.com>
2023-09-20 14:12:13 +0200

    [add]: const number and third point in js

--- js/script.js
+++ js/script.js
@@ -2,15 +2,19 @@
 const title = document.getElementById('title')
 const containerGame = document.querySelector('.container');
 
+const number = [];
 //ULTIMO stampare messaggio gioco
 // title.innerHTML = 'Complimenti/Scarso' 
 
 //2. creare un elemento dove far apparire i numeri randomici
 
 const numbersContainer = document.createElement ('div');
+numbersContainer.innerHTML= '1 2 3 4 5'
 containerGame.append(numbersContainer);
 
 console.log(numbersContainer);
+
+//3. funzione per i numeri random
 
 
 
